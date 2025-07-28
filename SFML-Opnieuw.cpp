229cd5b43f88e780ce4620cf2636122cc597d3be
simi-bit <simeonsimson7@gmail.com>
2025-07-28 20:52:13 -0300

    Delete SFML-Opnieuw.cpp

--- SFML-Opnieuw.cpp
+++
@@ -1,9 +0,0 @@
-#include "Headers/Visuals/Window.h"
-
-int main() {
-	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Test", sf::Style::None, sf::State::Fullscreen);
-	window.setVerticalSyncEnabled(true);
-	Window startGame(&window);
-	window.setFramerateLimit(60);
-	return 0;
-}
